2480b0dc679b19d6f552e663b63900bcc45fa3e1
Mudasir Ali <unarmudasir@gmail.com>
2026-08-20 18:18:22 +0500

    feat: add ApiConstants class for centralized API endpoint management

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,12 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String baseUrl = "https://dummyjson.com";
+
+  static const int connectionTimeout = 20;
+
+  static const String login = "/login";
+  static const String register = "/register";
+
+  static const String userProfile = "/users";
+}
